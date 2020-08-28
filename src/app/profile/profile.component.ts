6685771261f8d13f8fe50fb788539d3341dca114
matiaslera <matiasmal_94@hotmail.com>
2020-08-28 20:32:44 -0300

    se agrega perfil

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,10 +8,18 @@ import { UsersService } from '../services/users.service';
 })
 export class ProfileComponent implements OnInit {
 
+  nombre: string;
+  apellido: string;
+  user:string;
+  direccion: string;
+  telefono:number;
+  email:string;
+
   constructor(public servicioUsuario: UsersService) { }
 
   ngOnInit(): void {
     this.getUserLogged()
+    this.init()
   }
 
   getUserLogged() {
@@ -19,4 +27,14 @@ export class ProfileComponent implements OnInit {
       console.log(user);
     });
   }
+
+  init(){
+    this.nombre= "Omar";
+  this.apellido="Castillo";
+  this.user="omar_castillo_20"
+  this.direccion="av siempre viva"
+  this.telefono= 15423589
+  this.email= "omarcastio20@gmail.com"
+  }
+
 }
