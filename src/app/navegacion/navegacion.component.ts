48ffe0a324ba4d3513c1970029130923f0a7a888
matiaslera <matiasmal_94@hotmail.com>
2020-09-03 01:47:31 -0300

    se termino la navegacion

--- src/app/navegacion/navegacion.component.ts
+++ src/app/navegacion/navegacion.component.ts
@@ -16,20 +16,5 @@ export class NavegacionComponent implements OnInit {
   userIsLogged():boolean{
     return this.servicioUsuario.logueado
   }
-  /* openNav() {
-   var x= document.getElementById("mySidebar")
-  x.style.width = "180px";
-  x.style.minWidth = "18%";
-  var y =document.getElementById("main")
-    y.style.display="none"
-}
-
-  closeNav() {
-    var x =document.getElementById("mySidebar")
-    var y =document.getElementById("main")
-  x.style.width = "0";
-  x.style.minWidth = "0";
-  x.style.marginLeft= "0";
-    y.style.display="flex" 
-} */
+ 
 }
